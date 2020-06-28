930369d6351bf04a8a54a5b63a70dbc3ec1b2573
MDRCS <elrahali.md@gmail.com>
2020-06-28 12:02:20 +0100

    Added views api

--- src/django_project/django_restapi/views.py
+++ src/django_project/django_restapi/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloAPIVIEW(APIView):
+
+    def get(self, request, format=None):
+        users = ['Mohamed El Rahali',
+                 'Anas Eit Lhioui']
+
+        return Response({'message': 'hello', 'users': users})
