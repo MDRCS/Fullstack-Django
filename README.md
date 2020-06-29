5988b536ee4e42409dfe3afa3cb4170526dc39c5
MDRCS <elrahali.md@gmail.com>
2020-06-29 15:14:08 +0100

    Restrict No Logged Users to read-only

--- README.md
+++ README.md
@@ -76,3 +76,4 @@
 
     $ python manage.py makemigrations
     $ python manage.py migrate
+
--- src/django_project/django_restapi/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoRestapiConfig(AppConfig):
-    name = 'django_restapi'
