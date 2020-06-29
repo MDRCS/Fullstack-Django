137482f545c9adc5e8c48fca99a5caadeebff105
MDRCS <elrahali.md@gmail.com>
2020-06-29 11:26:28 +0100

    Added Permissions and Auth to UserProfile Model

--- src/django_project/django_restapi/views.py
+++ src/django_project/django_restapi/views.py
@@ -4,7 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status  # http status
 from . import serializers
 from . import models
-
+from . import permissions
+from rest_framework.authentication import TokenAuthentication
 
 class HelloAPIVIEW(APIView):
     serializer_class = serializers.HelloSerializer
@@ -73,4 +74,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
 
-
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
